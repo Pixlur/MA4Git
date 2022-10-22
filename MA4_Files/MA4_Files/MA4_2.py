d31ae7d8344c5a060c6b4e6a03e591286d2c84a0
Pixlur <alexander@svartbacken.nu>
2022-10-22 11:40:33 +0200

    Update MA4_2.py

--- MA4_Files/MA4_Files/MA4_2.py
+++ MA4_Files/MA4_Files/MA4_2.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 from person import Person
+from numba import njit
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))
+
+@njit
+def fib_numba(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_numba(n-1) + fib_numba(n-2))
 
 def main():
 	f = Person(5)
@@ -8,7 +22,7 @@ def main():
 	f.set(1)
 	print(f.get())
 	print(f.fib())
-	f.set(6)
+	f.set(47)
 	print(f.fib())
 if __name__ == '__main__':
 	main()
